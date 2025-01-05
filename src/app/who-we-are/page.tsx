441dd8c5cfc5d83a2391f2cb1ee3cd7d5f8de44d
Sam Heyman <sam.heyman@gmail.com>
2025-01-05 18:55:21 +0100

    Update Who We Are page.tsx

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -1,1 +1,15 @@
+import WhoWeAre from "../components/WhoWeAre";
+import Betty from "../components/Betty";
+import CasaPueblito from "../components/CasaPueblito";
+import JoinUs from "../components/JoinUs";
 
+export default function Home() {
+  return (
+    <>
+      <WhoWeAre />
+      <Betty />
+      <CasaPueblito />
+      <JoinUs />
+    </>
+  );
+}
