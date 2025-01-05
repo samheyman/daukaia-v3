963cc2e7935723a7bcc89857927ac6218188d80f
Sam Heyman <sam.heyman@gmail.com>
2025-01-05 15:00:05 +0100

    Create what-we-do.tsx

---
+++ src/app/pages/what-we-do.tsx
@@ -0,0 +1,17 @@
+import WhatWeDo from "./components/WhatWeDo";
+import DaukaiaMeaning from "./components/DaukaiaMeaning";
+import Betty from "./components/Betty";
+import CasaPueblito from "./components/CasaPueblito";
+import JoinUs from "./components/JoinUs";
+
+export default function Home() {
+  return (
+    <>
+      <WhatWeDo />
+      <DaukaiaMeaning />
+      <Betty />
+      <CasaPueblito />
+      <JoinUs />
+    </>
+  );
+}
