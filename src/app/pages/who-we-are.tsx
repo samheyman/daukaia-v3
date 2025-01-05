460377ef98bc88864d9a6bfadceababefcb84e88
Sam Heyman <sam.heyman@gmail.com>
2025-01-05 15:09:43 +0100

    Update import paths

--- src/app/pages/who-we-are.tsx
+++ src/app/pages/who-we-are.tsx
@@ -1,7 +1,7 @@
-import WhoWeAre from "./components/WhoWeAre";
-import Betty from "./components/Betty";
-import CasaPueblito from "./components/CasaPueblito";
-import JoinUs from "./components/JoinUs";
+import WhoWeAre from "../components/WhoWeAre";
+import Betty from "../components/Betty";
+import CasaPueblito from "../components/CasaPueblito";
+import JoinUs from "../components/JoinUs";
 
 export default function Home() {
   return (
