67b3bc670d22f2b7afb4817650eed0bf425a914c
Sam Heyman <sam.heyman@gmail.com>
2025-01-16 21:19:49 +0100

    Change font for stats

--- src/app/components/Stats.tsx
+++ src/app/components/Stats.tsx
@@ -1,27 +1,29 @@
+import { newseader } from "../fonts";
+
 export default function Stats() {
   return (
     <section className=" bg-slate-100 ">
       <dl className="my-8 py-8 py-4 wrapper flex flex-row justify-evenly">
         <div className="m-8 flex-1">
-          <dd className="text-5xl text-center">500</dd>
+          <dd className={`${newseader.className} text-5xl text-center`}>500</dd>
           <dt className="text-lg text-center pt-2">
             children empowered since 2010
           </dt>
         </div>
         <div className="m-8 flex-1">
-          <dd className="text-5xl text-center">65%</dd>
+          <dd className={`${newseader.className} text-5xl text-center`}>65%</dd>
           <dt className="text-lg text-center pt-2">
             of beneficiaires achieve higher grades
           </dt>
         </div>
         <div className="m-8 flex-1">
-          <dd className="text-5xl text-center">34</dd>
+          <dd className={`${newseader.className} text-5xl text-center`}>34</dd>
           <dt className="text-lg text-center pt-2">
             international volunteers engaged
           </dt>
         </div>
         <div className="m-8 flex-1">
-          <dd className="text-5xl text-center">50+</dd>
+          <dd className={`${newseader.className} text-5xl text-center`}>50+</dd>
           <dt className="text-lg text-center pt-2">
             teachers trained in local schools
           </dt>
