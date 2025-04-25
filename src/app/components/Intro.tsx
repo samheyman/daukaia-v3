5f154d6547353a30c8d3d55eec0ddc822a23577c
Sam Heyman <sam.heyman@gmail.com>
2025-04-25 11:19:19 +0200

    Update wording of intro paragraph

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -5,8 +5,8 @@ export default function Intro() {
     <section className={`meaning ${newsreader.className} text-center`}>
       <p className="text-3xl max-w-4xl mx-12 lg:mx-0">
         We are a small, grassroots nonprofit working with children and families
-        in Ocotal, Nicaragua. We create safe spaces for learning, play, growth,
-        and healing. The kind of spaces every child deserves.
+        in Ocotal, Nicaragua. We provide a safe space for learning, play,
+        growth, and healing. The kind of spaces every child deserves.
       </p>
     </section>
   );
